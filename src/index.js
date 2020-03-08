743f756e22f05401cb3c166f9a1a988c5186c9db
allihach <lihach.ac@gmail.com>
2020-03-08 16:55:24 +0400

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    let min = Infinity;
+
+    if(array==undefined || array.length==0){
+        return 0;
+    }
+    
+    for(let i=0; i<array.length; i++) {
+        if(array[i]<min) {
+            min=array[i];
+        }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let max = -Infinity;
+    
+    if(array==undefined || array.length==0){
+        return 0;
+    }
+    
+    for(let i=0; i<array.length; i++) {
+        if(array[i]>max) {
+            max=array[i];
+        }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avg = 0;
+
+    if(array==undefined || array.length==0){
+        return 0;
+    }
+    for(let i=0; i<array.length; i++) {
+        avg += array[i];
+    }
+    return avg = avg/array.length;
 }
